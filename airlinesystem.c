885a0de8278739e9e465a47a91b22a31374c1a10
emanjamshed <emanjamshed8@gmail.com>
2022-11-01 10:31:56 +0500

    added function

--- airlinesystem.c
+++ airlinesystem.c
@@ -23,5 +23,87 @@ int main()
 
 }
 
+class(int array[])
+{
+	printf("\n      Type 1: FIRST CLASS\n");
+	printf("      Type 2: ECONOMY CLASS\n");
+
+	for (int j = 0; j < 100; j++)
+	{
+		printf("\nSelect type of class (1 or 2)\n or select 3 to exits: ");
+		int num;
+		int seat;
+		scanf_s("%d", &num);
+		while (num > 2 || num < 1)
+		{
+			printf(" No such class availabe");
+			return 0;
+		}
+
+
+		if (num == 1)
+		{
+			printf("Select your seat number (1-30): ");
+
+			scanf_s("%d", &seat);
+
+			while (seat > 30 || seat < 1)
+			{
+				printf("Please select between 1-30: ");
+				scanf_s("%d", &seat);
+
+
+			}
+
+			if (array[seat] == 1)
+			{
+				puts("SEAT IS RESERVED!!");
+			}
+			seat = array[seat] = 1;
+			int j = 0;
+			for (int i = 0; i < 100; i++)
+			{
+				j = j + 1;
+				printf("%d", array[i]);
+
+				if (j != 0 && j % 10 == 0)
+				{
+
+					puts("");
+				}
+			}
+		}
+		if (num == 2)
+		{
+			printf("Select your seat number (31-100): ");
+			scanf_s("%d", &seat);
+
+			while (seat > 100 || seat < 31)
+			{
+				printf("Please select between 31-100: ");
+				scanf_s("%d", &seat);
+
+			}
+			if (array[seat] == 1)
+			{
+				puts("SEAT IS RESERVED!!");
+			}
+			seat = array[seat] = 1;
+			int j = 0;
+			for (int i = 0; i < 100; i++)
+			{
+				j = j + 1;
+				printf("%d", array[i]);
+
+				if (j != 0 && j % 10 == 0)
+				{
+
+					puts("");
+				}
+			}
+
+		}
+	}
+}
 
 
